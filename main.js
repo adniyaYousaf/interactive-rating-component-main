0b76feaa0bf424d540f799889c11ba772e31d836
adniya <adnya@live.com>
2023-06-01 22:18:29 +0100

    fix: js

---
+++ main.js
@@ -0,0 +1,35 @@
+const submit = document.getElementById('submit');
+const reaction = document.getElementById('reaction');
+const maincard = document.getElementById('main-card');
+const input = document.querySelectorAll('input');
+const msg = document.getElementById('msg');
+let x;
+
+input.forEach(e => e.addEventListener('click', () =>
+    x = e.value
+));
+
+submit.addEventListener('click', react);
+
+function react() {
+    reaction.setAttribute('aria-hidden', 'false');
+    maincard.setAttribute('aria-hidden', 'true');
+
+    switch (x) {
+        case "1":
+            msg.innerText = "You selected 1 out of 5";
+            break;
+        case "2":
+            msg.innerText = "You selected 2 out of 5";
+            break;
+        case "3":
+            msg.innerText = "You selected 3 out of 5";
+            break;
+        case "4":
+            msg.innerText = "You selected 4 out of 5";
+            break;
+        case "5":
+            msg.innerText = "You selected 5 out of 5";
+            break;
+    }
+}
