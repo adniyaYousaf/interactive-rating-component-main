ca3a8fafe067c502590c4bcb054754e8b01161c4
adniya <adnya@live.com>
2023-06-01 22:53:12 +0100

    fix: js

--- main.js
+++ main.js
@@ -9,31 +9,37 @@ input.forEach(e => e.addEventListener('click', () =>
     x = e.value
 ));
 
+
 submit.addEventListener('click', react);
 
 function react() {
-    reaction.setAttribute('aria-hidden', 'false');
-    maincard.setAttribute('aria-hidden', 'true');
-
-    switch (x) {
-        case "1":
-            msg.innerText = "You selected 1 out of 5";
-            break;
-        case "2":
-            msg.innerText = "You selected 2 out of 5";
-            break;
-        case "3":
-            msg.innerText = "You selected 3 out of 5";
-            break;
-        case "4":
-            msg.innerText = "You selected 4 out of 5";
-            break;
-        case "5":
-            msg.innerText = "You selected 5 out of 5";
-            break;
-            case " ":
-                msg.innerText = "You selected none";
-                break;
-    }
+    console.log(x);
+    if(x==null){
+        alert("Select a number");
+    }else{
+        reaction.setAttribute('aria-hidden', 'false');
+        maincard.setAttribute('aria-hidden', 'true');
     
+        switch (x) {
+            case "1":
+                msg.innerText = "You selected 1 out of 5";
+                break;
+            case "2":
+                msg.innerText = "You selected 2 out of 5";
+                break;
+            case "3":
+                msg.innerText = "You selected 3 out of 5";
+                break;
+            case "4":
+                msg.innerText = "You selected 4 out of 5";
+                break;
+            case "5":
+                msg.innerText = "You selected 5 out of 5";
+                break;
+    
+        }
+    
+    }
+   
+
 }
